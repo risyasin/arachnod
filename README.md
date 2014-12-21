2686a99eaa1377747117deeb41952e07f78a867c
yasin inat <risyasin@gmail.com>
2014-12-21 14:38:34 +0200

    upd.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-#### Nodejs Crawler
+### Arachnod
+#### High performance crawler for Nodejs
+
+Powerful & Easy to use web crawler for Nodejs.  [Arachnod](http://arachnod.evrima.net) has been designed for heavy long runing tasks, 
+for performance & efective resource usage. For it's goals Arachnod uses [Redis](http://www.redis.io)'s power as a backend. 
+Covering all heavy & time consuming tasks such as controlling urls & their tasks to store & distribute information among the Arachnod's child tasks 
+(Spiderlings). Arachnod also avoids to use any server-side DOM requiring technics 
+such as [jQuery](http://www.jquery.com) with [JSdom](https://github.com/tmpvar/jsdom) to use resources properly. 
+Frankly tested JSdom for along time with no luck, always memory leaks & high memory usage. 
+Libxml based XPath solutions were not actually real. Arachnod uses [Cheerio](http://cheeriojs.github.io/cheerio/) for accessing DOM elements. 
+Also uses [SuperAgent](https://github.com/visionmedia/superagent) as HTTP Client. 
+
+
+###### How to install 
+* via NPM 
+    npm install arachnod
+    
+* via Git 
+    git clone http://git.evrima.net/admin/projects/dev/arachnod
--- index.js
+++ index.js
@@ -1,92 +1,202 @@
 "use strict";
 
-var os      = require("os"),
-    cp      = require("child_process"),
-    ee      = require("eventemitter2").EventEmitter2,
-    _       = require("underscore"),
+var redis   = require("redis"),
+    Event   = require("eventemitter2").EventEmitter2,
+    Agent   = require("superagent"),
+    Url     = require("url"),
     cheerio = require("cheerio"),
-    Arch    = { "version": "0.1.0", "cfg": {} };
+    _       = require("underscore"),
+    defKey  = process.cwd().split("/").slice(-1) || 'arachnod';
 
-module.exports = (function () {
-    var status = {
+module.exports = function (rs) {
+
+    var Arch = new Event({
+            delimiter: '::',
+            newListener: false,
+            maxListeners: 20
+        }),
+        rc = redis.createClient(6379, rs),
+        status = {
             "set": "init",
             "startTime": Date.now(),
             "stop": false
         },
-        slings = {},
-        userf = {};
+        cache = [],
+        tasks = defKey + '_tasks',
+        finished = defKey + '_finished',
+        cfg = {
+            "resume": false,
+            "sameDomain": true,
+            "useCookies": true,
+            "userAgents": {
+                "googlebot": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+                "yandex": "Mozilla/5.0 (compatible; YandexBot/3.0; +http://yandex.com/bots)",
+                "bing": "Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)",
+                "arachnod": "Mozilla/5.0 (compatible; Arachnod/0.1; Bot/0.1; +http://arachnod.evrima.net/)",
+                "firefox": "Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0",
+                "chrome": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+                "ie": "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)",
+                "ie10": "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)",
+                "ie9": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 7.1; Trident/5.0)",
+                "ie8": "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; GTB7.4; InfoPath.2; SV1; .NET CLR 3.3.69573; WOW64; en-US)",
+                "ie7": "Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)",
+                "ie6": "Mozilla/4.0 (compatible; MSIE 6.01; Windows NT 6.0)",
+                "opera": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36 OPR/15.0.1147.100",
+                "safari": "Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25"
+            }
+        };
 
-    Arch.init = function () {
-        Arch.cfg.cpus = os.cpus();
+    Arch.headers = (function () {
+        // @Todo: Auto generation of Accept headers by task
+        // @Todo: Auto generation of Accept=Language headers by configuration
+        return {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding": "gzip,deflate,sdch",
+            "Accept-Language": "en-US,en;q=0.8,tr;q=0.6",
+            "User-Agent": cfg.userAgents.arachnod,
+            "Referer": "http://www.google.com/",
+            "Cache-Control": "no-cache, no-store, must-revalidate",
+            "Pragma": "no-cache",
+            "Expires": 0
+        };
+    }());
 
-        try {
-            var defaults = require(__dirname + "/defaults.json");
-            Arch.cfg = _.extend(Arch.cfg, defaults);
-        } catch(e) { throw console.log(['Unable to read defaults file [defaults.json]', e]); }
-
-        Arch.rc = redis.createClient(Arch.cfg.redis.port, Arch.cfg.redis.server, Arch.cfg.redis.options);
-
-        if (!Arch.cfg.options.resume){
-            Arch.rc.del(Arch.cfg.tasks, function (err, rt) {
-                if (err) { throw err; }
-                Arch.rc.del(Arch.cfg.finished, function (err, rf) {
-                    if (err) { throw err; }
-                    console.log('Previous task canceled! ' + rt + ' - ' + rf);
+    Arch.queue = function (taskData) {
+        if (taskData.url){
+            if (_.indexOf(cache, taskData.url) === -1){
+                var taskStr = JSON.stringify(taskData);
+                rc.sismember(finished, taskStr, function (err, res) {
+                    if (!res){
+                        rc.sadd(tasks, taskStr);
+                        cache.push(taskData.url);
+                    }
                 });
-            });
-        }
-    };
-
-    Arch.forkMsg = function (m, pid) {
-        if (m.doc && m.text){
-            userf.hit(null, m.doc , cheerio.load(m.text));
-        }
-        if (m.cmd && m.cmd === 'end'){
-            if (_.keys(slings).length > 1){
-                pid.kill();
-            } else {
-                userf.end(null, Arch.finished);
             }
         }
     };
 
-    Arch.crawl = function () {
-        status.set = "forking";
-        _.each(Arch.cfg.cpus, function (cpu, i) {
-            // console.log('forked ' + i);
-            slings[i] = cp.fork(__dirname + "/spiderling.js");
-            slings[i].on('message', function (m) {
-                Arch.forkMsg(m, slings[i].pid);
-                m = null;
+
+    Arch.finish = function (taskData) {
+        if (taskData.url){
+            rc.sadd(finished, JSON.stringify(taskData));
+            Arch.checkTasks();
+        }
+    };
+
+
+    Arch.checkTasks = function () {
+        rc.scard(tasks, function (err, tl) {
+            rc.scard(finished, function (err, fl) {
+                if (fl === tl){ process.send({ cmd: 'end' }); }
             });
         });
-        // Arch.checkTasks();
     };
 
-    Arch[status.set]();
-
-    process.on('SIGINT', function() {
-        _.each(slings, function (cp, i) { cp.kill(); });
-        process.exit();
-    });
-
-    return {
-        "crawl": function (params) {
-            Arch.cfg.options = _.extend(Arch.cfg.options, params.cfg);
-            if (typeof params.start === "string"){
-                Arch.rc.sadd(Arch.cfg.tasks, JSON.stringify({ "url": params.start}));
+    Arch.getTask = function (cb) {
+        rc.spop(tasks, function (err, task) {
+            if (task !== null){
+                task = JSON.parse(task);
+                cb(task);
+            } else {
+                // retry
+                setTimeout(Arch.run, 500);
             }
-            if (_.isArray(params.start)){
-                _.each(params.start, function (val) {
-                    Arch.rc.sadd(Arch.cfg.tasks, JSON.stringify({ "url": val}));
+        });
+    };
+
+    Arch.setStatus = function (msg) {
+        status.set = msg;
+    };
+
+    Arch.runTask = function (task, cb) {
+        // var url = Url.parse(task.url);
+        status.set = "downloading";
+        Agent.get(task.url)
+            .set(Arch.headers)
+            .end(function(result){
+                cb(true, task, result);
+                result = null;
+            });
+    };
+
+    Arch.taskRetry = function (task, code) {
+        task.lastResponse = code;
+        if (task.retry){ task.retry++; } else { task.retry = 1; }
+        Arch.setStatus("retrying");
+        setTimeout(function () {
+            Arch.runTask(task, Arch.processResponse);
+        }, 10000);
+    };
+
+    Arch.processResponse = function (success, task, result) {
+        if (task.url && result.res.statusCode === 200){
+            //console.log([success, result.res.statusCode, result.res.headers, result.res.text]);
+            Arch.setStatus("parsing " + task.url);
+            try {
+                var $ = cheerio.load(result.res.text),
+                    taskp = Url.parse(task.url),
+                    doc = {
+                        "url": task.url,
+                        "parsed": taskp,
+                        "headers": result.res.headers
+                    };
+                _.each($("a"), function (item, i) {
+                    var href = item.attribs.href, urlp = Url.parse(href);
+                    if (urlp.host === taskp.host){ // same domain
+                        Arch.queue({"url": href});
+                    }
                 });
+                Arch.emit('hit', doc, $, result.res.text);
+                Arch.finish(task);
+                doc = null;
+            } catch(e) {
+                console.log(['processResponse failed', e, task]);
+                //Arch.taskRetry(task, result.res.statusCode);
+            } finally {
+                Arch.setStatus("idle");
+                Arch.run();
             }
-            userf = _.extend(userf, params);
-            //console.log(['crawl call', slings]);
-            Arch.crawl();
+        } else {
+            Arch.taskRetry(task, result.res.statusCode);
         }
+        result = null;
     };
 
-}());
+    Arch.run = function () {
+        Arch.setStatus("running");
+        Arch.getTask(function (task) {
+            if (typeof task !== 'undefined' && task.url){
+                Arch.runTask(task, Arch.processResponse);
+            } else {
+                // while idle.
+                Arch.setStatus("idle");
+                setTimeout(Arch.run, 500);
+            }
+        });
+    };
 
-process.on("uncaughtException", function (err) { console.log(["ue", err.stack]); });
+    
+    Arch.reset = function () {
+        rc.del(tasks, function (err, rt) {
+            if (err) { throw err; }
+            rc.del(finished, function (err, rf) {
+                if (err) { throw err; }
+                console.log('Previous task canceled! ' + rt + ' - ' + rf);
+            });
+        });
+    };
+
+    Arch.crawl = function (url) {
+        console.log('started to crawl!');
+        Arch.queue({"url": url});
+        Arch.run();
+        //Arch.emit('error', 123, {"test": test});
+
+    };
+    return Arch;
+};
+
+
+process.on("uncaughtException", function (err) {
+    console.log(err.stack);
+});
